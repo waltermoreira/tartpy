fd38f23a0068b3411568ff1530ea0531b28e0a82
Walter Moreira <walter@waltermoreira.net>
2014-01-06 23:44:24 -0600

    Document eventloop module

--- eventloop.py
+++ eventloop.py
@@ -1,3 +1,21 @@
+"""
+
+Very basic implementation of an event loop
+==========================================
+
+Exports
+-------
+
+- ``ThreadedEventLoop``: global event loop running in a thread
+
+- ``ManualEventLoop``: global event loop to be run synchronously
+
+- ``individual_loop_step``: process one event
+
+- ``individual_loop``: process events indefinitely
+
+"""
+
 import queue
 import sys
 import threading
@@ -5,6 +23,7 @@ import traceback
 
 
 def _format_exception(exc_info):
+    """Create a message with details on the exception."""
     exc_type, exc_value, exc_tb = exc_info
     return {'exception': {'type': exc_type,
                           'value': exc_value,
@@ -13,6 +32,13 @@ def _format_exception(exc_info):
     
 
 def individual_loop_step(queue, actor, block=True):
+    """Process one event for ``actor``.
+
+    The argument ``block`` decides whether to block or not when there
+    are no messages.  Any exception in the behaviors is reported
+    through the error mechanism of the actor.
+
+    """
     message = queue.get(block=block)
     try:
         return actor.behavior(message)
@@ -23,11 +49,17 @@ def individual_loop_step(queue, actor, block=True):
 
     
 def individual_loop(queue, actor):
+    """Process events for ``actor`` indefinitely."""
     while True:
         individual_loop_step(queue, actor)
 
         
 def global_loop_step(queue, block=False):
+    """Global event loop step.
+
+    Process one event extracted from the queue.
+    
+    """
     actor, message = queue.get(block=block)
     try:
         actor.behavior(message)
@@ -36,11 +68,19 @@ def global_loop_step(queue, block=False):
 
 
 def global_loop(queue):
+    """Process events indefinitely."""
     while True:
         global_loop_step(queue, block=True)
 
 
 class EventLoop(object):
+    """A generic event loop object.
+
+    An ``EventLoop`` is a singleton.  Get an instance with::
+
+        EventLoop.get_loop()
+
+    """
 
     loop = None
 
@@ -48,16 +88,23 @@ class EventLoop(object):
         self.queue = queue.Queue()
 
     def schedule(self, message, target):
+        """Schedule an event."""
         self.queue.put((target, message))
 
     @classmethod
     def get_loop(cls):
+        """Obtain a loop.
+
+        Start a new one if necessary.
+
+        """
         if cls.loop is None:
             cls.loop = cls()
         return cls.loop
         
 
 class ThreadedEventLoop(EventLoop):
+    """An event loop that dispatches events from a thread."""
 
     def __init__(self):
         super().__init__()
@@ -69,8 +116,14 @@ class ThreadedEventLoop(EventLoop):
 
 
 class ManualEventLoop(EventLoop):
+    """An event loop that needs to be run explicitly."""
 
     def run(self):
+        """Process all events in the queue until empty.
+
+        It doesn't block if the queue is empty.
+
+        """
         try:
             while True:
                 global_loop_step(self.queue)
