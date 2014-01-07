84f8575d2d2d8586b594fad5900266d4e46e6a3e
Walter Moreira <walter@waltermoreira.net>
2014-01-07 10:48:13 -0600

    Comments for sponsor

--- sponsor.py
+++ sponsor.py
@@ -1,14 +1,31 @@
+"""
+
+Sponsor objects
+===============
+
+Sponsors are objects that can manage resources for actor allocations.
+Creation and error reporting can be delegated to them when an actor is
+create with a keyword argument of the form::
+
+    Actor.create(sponsor=obj)
+
+"""
+
 class AbstractSponsor(object):
+    """Interface for sponsors."""
 
     def create(self, cls, **kwargs):
+        """Create an actor from ``cls``."""
         raise NotImplementedError()
 
     def error(self, actor, message):
+        """Report error from ``actor``."""
         raise NotImplementedError()
         
 
 class SimpleSponsor(AbstractSponsor):
-
+    """Sponsor that keeps track of created actors."""
+    
     def __init__(self):
         self.actors = []
         
