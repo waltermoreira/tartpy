92a0ecc13db9cecc2031536f257dcd826ef919ca
Walter Moreira <walter@waltermoreira.net>
2014-01-13 21:52:29 -0600

    Add tests as a subcommand for setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
 import os
+import sys
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
 
 setup(name='tartpy',
       version='0.1.0',
@@ -12,7 +24,9 @@ setup(name='tartpy',
       packages=find_packages(),
       license='MIT',
       long_description=open('README.rst').read(),
+      cmdclass = {'test': PyTest},
       install_requires=[
           "pytest >= 2.4",
-      ]
+      ],
+      tests_require=['pytest']
 )
