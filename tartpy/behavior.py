7227fd65e4b97aae5a8ba650b74526cfa59b69c4
Walter Moreira <walter@waltermoreira.net>
2014-02-14 00:26:56 -0600

    Exploring sponsors and contexts

--- tartpy/behavior.py
+++ tartpy/behavior.py
@@ -2,6 +2,7 @@ import threading
 import time
 import queue
 from functools import wraps, partial
+import sys
 
 from . import eventloop
 
@@ -15,14 +16,24 @@ def behavior(f):
     return wrapper
 
 @behavior
-def bar(self, msg):
+def bar(context, msg):
     print("i'm bar", msg)
 
 @behavior
-def foo(n, self, msg):
-    print(n, self, msg)
-    self.become(bar())
+def foo(n, context, msg):
+    print(n, context, msg)
+    context.become(bar)
 
+@behavior
+def gii(context, n):
+    print('gii got', n)
+    if n > 0:
+        context.self << n-1
+    else:
+        print('finally')
+        a = context.sponsor(foo, 3)
+        a << 5
+    
 q = queue.Queue()
 
 def beh_loop(queue, block=True):
@@ -47,10 +58,18 @@ def run_loop():
 #     args=(queue,),
 #     name='beh_loop')
 #loop.start()
+
+
+def sponsor(behavior, *args):
+    a = Actor(sponsor, behavior, *args)
+    return Capability(a.send)
+
     
 class Actor(object):
 
-    def __init__(self, behavior, *args):
+    def __init__(self, sponsor, behavior, *args):
+        self.sponsor = sponsor
+        self.self = Capability(self.send)
         self.become(behavior, *args)
 
     def become(self, behavior, *args):
@@ -59,130 +78,154 @@ class Actor(object):
     def send(self, msg):
         q.put((self, msg))
 
+
+class Capability(object):
+
+    def __init__(self, f):
+        self.f = f
+
     def __lshift__(self, msg):
-        self.send(msg)
-        
+        self.f(msg)
+
 
 def test_receive_message():
 
+    result = None
+    
     @behavior
-    def beh(self, msg):
-        self.message = msg
+    def beh(context, msg):
+        nonlocal result
+        result = msg
 
-    a = Actor(beh)
+    a = sponsor(beh)
     a << 5
     run_loop()
-    assert a.message == 5
+    assert result == 5
 
 def test_create_with_args():
 
+    result = None
+    
     @behavior
-    def beh(arg, self, msg):
-        self.arg = arg
+    def beh(arg, context, msg):
+        nonlocal result
+        result = arg
 
-    a = Actor(beh, True)
+    a = sponsor(beh, True)
     a << 0
     run_loop()
-    assert a.arg is True
+    assert result is True
 
 def test_one_shot():
 
+    sink_beh_done = False
+    destination_beh_done = False
+    message = None
+    
     @behavior
-    def one_shot_beh(destination, self, msg):
-        destination.send(msg)
-        self.become(sink_beh)
+    def one_shot_beh(destination, context, msg):
+        destination << msg
+        context.become(sink_beh)
 
     @behavior
-    def sink_beh(self, msg):
+    def sink_beh(context, msg):
         assert msg == 'second'
-        self.sink_beh_done = True
+        nonlocal sink_beh_done
+        sink_beh_done = True
 
     @behavior
-    def destination_beh(self, msg):
-        self.msg = msg
-        self.destination_beh_done = True
+    def destination_beh(context, msg):
+        nonlocal message, destination_beh_done
+        message = msg
+        destination_beh_done = True
 
-    destination = Actor(destination_beh)
-    one_shot = Actor(one_shot_beh, destination)
+    destination = sponsor(destination_beh)
+    one_shot = sponsor(one_shot_beh, destination)
 
     one_shot << 'first'
     one_shot << 'second'
 
     run_loop()
-    assert destination.msg == 'first'
-    assert one_shot.sink_beh_done and destination.destination_beh_done
+    assert message == 'first'
+    assert sink_beh_done and destination_beh_done
     
 def test_serial():
 
+    first_msg = second_msg = third_msg = None
+    first_behavior = second_behavior = third_behavior = None
+    first = second = third = None
+    
     @behavior
-    def first_beh(self, msg):
-        self.become(second_beh)
-        self.first_msg = msg
-        self.first_behavior = record(self)
-        self.first = True
+    def first_beh(context, msg):
+        context.become(second_beh)
+        nonlocal first_msg, first_behavior, first
+        first_msg = msg
+        first_behavior = record()
+        first = True
 
     @behavior
-    def second_beh(self, msg):
-        self.become(third_beh)
-        self.second_msg = msg
-        self.second_behavior = record(self)
-        self.second = True
+    def second_beh(context, msg):
+        context.become(third_beh)
+        nonlocal second_msg, second_behavior, second
+        second_msg = msg
+        second_behavior = record()
+        second = True
 
     @behavior
-    def third_beh(self, msg):
-        self.third_msg = msg
-        self.third_behavior = record(self)
+    def third_beh(context, msg):
+        nonlocal third_msg, third_behavior
+        third_msg = msg
+        third_behavior = record()
 
-    def record(actor):
-        return bool(actor.first), bool(actor.second), bool(actor.third)
+    def record():
+        return bool(first), bool(second), bool(third)
         
-    serial = Actor(first_beh)
-    serial.first = serial.second = serial.third = False
+    serial = sponsor(first_beh)
     serial << 'foo'
     serial << 'foo'
     serial << 'foo'
 
     run_loop()
 
-    assert serial.first_msg == 'foo' and serial.second_msg == 'foo' and serial.third_msg == 'foo'
-    assert serial.first_behavior == (False, False, False)
-    assert serial.second_behavior == (True, False, False)
-    assert serial.third_behavior == (True, True, False)
+    assert first_msg == 'foo' and second_msg == 'foo' and third_msg == 'foo'
+    assert first_behavior == (False, False, False)
+    assert second_behavior == (True, False, False)
+    assert third_behavior == (True, True, False)
 
 @behavior
-def ringlink_beh(next, self, n):
+def ringlink_beh(next, context, n):
     next << n
 
 @behavior
-def ringlast_beh(first, self, n):
+def ringlast_beh(first, context, n):
     loop_completion_times.append(time.time())
     if n > 1:
         first << n-1
     else:
-        self.become(sink_beh)
+        context.become(sink_beh)
         report()
 
 @behavior
-def sink_beh(self, msg):
+def sink_beh(context, msg):
     pass
 
 @behavior
-def ringbuilder_beh(m, self, msg):
+def ringbuilder_beh(m, context, msg):
     if m > 0:
-        next = Actor(ringbuilder_beh, m-1)
+        next = context.sponsor(ringbuilder_beh, m-1)
         next << msg
-        self.become(ringlink_beh, next)
+        context.become(ringlink_beh, next)
     else:
         global construction_end_time
         construction_end_time = time.time()
         msg['first'] << msg['n']
-        self.become(ringlast_beh, msg['first'])
+        context.become(ringlast_beh, msg['first'])
 
 def erlang_challenge(m, n):
     print('Starting {} actor ring'. format(m))
     global construction_start_time
     construction_start_time = time.time()
-    ring = Actor(ringbuilder_beh, m)
+    ring = sponsor(ringbuilder_beh, m)
     ring << {'first': ring, 'n': n}
 
 construction_start_time = 0
