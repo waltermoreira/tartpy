ea5f2e636b56d6516ed23858ab365e4240aedc52
Walter Moreira <walter@waltermoreira.net>
2014-02-22 15:52:45 -0600

    Rewrite erlang challenge for new syntax

--- tartpy/erlang_challenge.py
+++ tartpy/erlang_challenge.py
@@ -1,71 +1,69 @@
-import time
+import os
 import sys
-from rt import Actor, initial_behavior
+import time
+
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
+
+from tartpy.runtime import behavior, SimpleRuntime
+from tartpy.eventloop import EventLoop
 
 construction_start_time = 0
 construction_end_time = 0
 loop_completion_times = []
 
-class RingLink(Actor):
-    # args: next
 
-    @initial_behavior
-    def ringlink_beh(self, n):
-        self.next(n)
+@behavior
+def ringlink_beh(next, self, n):
+    next << n
 
+@behavior
+def ringlast_beh(first, self, n):
+    loop_completion_times.append(time.time())
+    if n > 1:
+        first << n-1
+    else:
+        self.become(sink_beh)
+        report()
 
-class RingLast(Actor):
-    # args: first
-    
-    @initial_behavior
-    def ringlast_beh(self, n):
-        loop_completion_times.append(time.time())
-        if n > 1:
-            self.first(n-1)
-        else:
-            self.behavior = self.sink_beh
-            self.report()
+@behavior
+def sink_beh(self, msg):
+    pass
 
-    def sink_beh(self, message):
-        pass
+@behavior
+def ringbuilder_beh(m, self, msg):
+    if m > 0:
+        next = self.create(ringbuilder_beh, m-1)
+        next << msg
+        self.become(ringlink_beh, next)
+    else:
+        global construction_end_time
+        construction_end_time = time.time()
+        msg['first'] << msg['n']
+        self.become(ringlast_beh, msg['first'])
 
-    def report(self):
-        print('Construction time: {} seconds'.format(
-            construction_end_time - construction_start_time))
-        print('Loop times:')
-        loop_completion_times.insert(0, construction_end_time)
-        intervals = [t1-t0
-                     for (t0, t1) in zip(loop_completion_times,
-                                         loop_completion_times[1:])]
-        for t in intervals:
-            print('  {} seconds'.format(t))
-        print('Average: {} seconds'.format(sum(intervals)/len(intervals)))
-
-
-class RingBuilder(Actor):
-    # args: m
-
-    @initial_behavior
-    def ringbuilder_beh(self, message):
-        if self.m > 0:
-            next = RingBuilder.create(m=self.m-1)
-            next(message)
-            self.behavior = RingLink(next=next).behavior
-        else:
-            global construction_end_time
-            construction_end_time = time.time()            
-            message['first'](message['n'])
-            self.behavior = RingLast(first=message['first']).behavior
-
-
-def test(m, n):
-    print('Starting {} actor ring'.format(m))
+def erlang_challenge(m, n):
+    print('Starting {} actor ring'. format(m))
     global construction_start_time
+    runtime = SimpleRuntime()
     construction_start_time = time.time()
-    ring = RingBuilder.create(m=m)
-    ring({'first': ring, 'n': n})
+    ring = runtime.create(ringbuilder_beh, m)
+    ring << {'first': ring, 'n': n}
+
+def report():
+    print('Construction time: {} seconds'.format(
+        construction_end_time - construction_start_time))
+    print('Loop times:')
+    loop_completion_times.insert(0, construction_end_time)
+    intervals = [t1-t0
+                 for (t0, t1) in zip(loop_completion_times,
+                                     loop_completion_times[1:])]
+    for t in intervals:
+        print('  {} seconds'.format(t))
+    print('Average: {} seconds'.format(sum(intervals)/len(intervals)))
+
 
 if __name__ == '__main__':
     m = int(sys.argv[1])
     n = int(sys.argv[2])
-    test(m, n)
+    erlang_challenge(m, n)
+    EventLoop().run()
