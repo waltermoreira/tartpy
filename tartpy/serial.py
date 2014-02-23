a6b3acaed26699d2d83e04f4f2027a07a6221ea1
Walter Moreira <walter@waltermoreira.net>
2014-02-23 00:07:21 -0600

    Fix serial to new notation

--- tartpy/serial.py
+++ tartpy/serial.py
@@ -1,26 +1,33 @@
-from .behavior import behavior, runtime
+from .runtime import behavior
 
 @behavior
-def serial_beh(actors, ctx, msg):
+def serial_beh(actors, self, msg):
     if actors:
-        tail = ctx.create(serial_beh, actors[1:])
+        tail = self.create(serial_beh, actors[1:])
         msg['reply_to'] = tail
         actors[0] << msg
 
 @behavior
-def add_beh(ctx, msg):
+def add_beh(self, msg):
     cust = msg['reply_to']
     msg['x'] += 1
     cust << msg
 
 @behavior
-def print_beh(ctx, msg):
+def print_beh(self, msg):
     print('got:', msg)
     
 def test():
+    from .runtime import SimpleRuntime
+    from .eventloop import EventLoop
+    
+    runtime = SimpleRuntime()
+
     pr = runtime.create(print_beh)
     a = runtime.create(add_beh)
     b = runtime.create(add_beh)
 
     x = runtime.create(serial_beh, [a, b, pr])
     x << {'x': 0}
+
+    EventLoop().run()
