44b3f05328ad074a4fe59033f9cc38676ffcbb50
Walter Moreira <walter@waltermoreira.net>
2014-02-28 16:01:24 -0600

    Add comments to runtime

--- tartpy/runtime.py
+++ tartpy/runtime.py
@@ -1,3 +1,40 @@
+"""Actor Runtime
+=============
+
+The runtime provides the functions to create an actor from a behavior,
+and to report an error.  Subclass from `AbstractRuntime` to create a
+runtime class.
+
+`SimpleRuntime.create` just creates the actor, and
+`SimpleRuntime.error` prints the error message to stdout.
+
+The behaviors are defined as::
+
+    @behavior
+    def my_beh(arg1, ..., self, msg):
+        ...
+
+where `arg1, ...` are 0 or more arguments to be passed at creation
+time. `self` refers to the actor in context, and `msg` is the message
+being passed.
+
+To create or change behavior, `self` provides the methods `create` and
+`become`. To send a message to an actor `x` use `x << msg`.  For
+example::
+
+    @behavior
+    def my_beh(n, self, msg):
+        if n > 0:
+            another = self.create(my_beh, n-1)
+            another << 'a message'
+        self.become(sink_beh)
+
+    @behavior
+    def sink_beh(self, msg):
+        pass
+
+"""
+
 from functools import wraps
 import pprint
 
@@ -45,10 +82,27 @@ class Actor(object):
         self.runtime.error(message)
 
     def __lshift__(self, msg):
+        """Syntax sugar for sending a message.
+
+        Use as `x << msg`.
+
+        """
         self.send(msg)
         
 
 def behavior(f):
+    """Decorator for declaring a function as behavior.
+
+    Use as::
+
+        @behavior
+        def fun(x, y, self, msg):
+            ...
+
+    And create or become this behavior by passing the two arguments
+    `x` and `y`.
+
+    """
     @wraps(f)
     def wrapper(*args):
         def _f(self, msg):
