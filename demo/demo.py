b5c69268ac1204dfa92872ee1b2cf23805388263
Walter Moreira <walter@waltermoreira.net>
2014-03-17 16:38:18 -0500

    Improve demo by writing explanations

--- demo/demo.py
+++ demo/demo.py
@@ -1,14 +1,46 @@
+"""Tartpy demo.
+
+Suggestions to play with this demo:
+
+- Install `tartpy`
+- Run `[i]python` from the `demo` directory, and `import demo`
+- Run already built tests (for example: `demo.test_counter()`)
+- Interact with the actors by sending them messages, for example::
+
+     counter << 5
+
+"""
+
 from tartpy.runtime import Runtime, behavior
 
+# The runtime is responsible for initially creating the actors
 runtime = Runtime()
 
+# ------------------------------
+
+# An actor in Python:
+# A behavior gets:
+# - `self`: the actor that is executing this behavior,
+# - `message`: the message being passed.
 
 @behavior
 def demo_beh(self, message):
     print(message)
 
+# Construct the actor with behavior `demo_beh`
+
 demo_actor = runtime.create(demo_beh)
 
+# and send it a message
+
+def test_demo_actor():
+    demo_actor << 'foo'
+
+# ------------------------------
+
+# An actor with state:
+# The first argument `state` is mutated for each message the actor
+# receives.
 
 @behavior
 def counter_beh(state, self, message):
@@ -19,8 +51,21 @@ def counter_beh(state, self, message):
 
     print('End count:  ', state['count'])
 
+# Create actor with inital state.
+    
 counter = runtime.create(counter_beh, {'count': 1337})
 
+# And test it with some messages
+
+def test_counter():
+    counter << 1
+    counter << 2
+    counter << -10
+
+# ------------------------------
+
+# Ping-pong example.
+# Notice the messages are asynchronous.
 
 @behavior
 def ping_beh(self, message):
@@ -42,18 +87,29 @@ def pong_beh(state, self, message):
 ping = runtime.create(ping_beh)
 pong = runtime.create(pong_beh, {'count': 2})
 
-ping << pong
+def test_ping_pong():
+    ping << pong
 
+# ------------------------------
 
+# Show methods available to the actor `self`.
+# In addition to `send`, `create`, and `become`,
+# the method `throw` raises an error to the runtime.
+    
 @behavior
 def show_self_beh(self, message):
-    print(self)
-    print(dir(self))
+    print('self:', self)
+    print('methods:', [meth for meth in dir(self) if not meth.startswith('_')])
 
 show_self = runtime.create(show_self_beh)
-show_self << None
 
+def test_show_self():
+    show_self << None
 
+# ------------------------------
+
+# Simple example of `become`
+    
 @behavior
 def say_red_beh(self, message):
     print('[SAYING]: red')
@@ -65,9 +121,14 @@ def say_black_beh(self, message):
     self.become(say_red_beh)
 
 say = runtime.create(say_red_beh)
-say << None
 
+def test_say():
+    say << None
 
+# ------------------------------
+
+# Changing behaviors with state:
+    
 @behavior
 def flip_up_beh(state, self, message):
     print(state, 'flipping up...')
@@ -80,4 +141,8 @@ def flip_down_beh(state, self, message):
 
 flipper = runtime.create(flip_down_beh, 'neither')
 
+def test_flipper():
+    flipper << None
+    flipper << None
+    flipper << None
 
