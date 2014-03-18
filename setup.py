6f08944fa2e73e00b00fd7fc130c058cfeece580
Walter Moreira <walter@waltermoreira.net>
2014-03-17 19:53:57 -0500

    Fix running tests with 'setup.py test'
    Ignore the build directory, since it contains a set of tests that are not supposed to be run.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools.command.test import test as TestCommand
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = []
+        self.test_args = ['--ignore', 'build']
         self.test_suite = True
     def run_tests(self):
         import pytest
