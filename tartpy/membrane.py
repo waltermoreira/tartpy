0fa28a8e408b5409b8acc21feb117a47612dc7ae
Walter Moreira <walter@waltermoreira.net>
2014-04-13 23:34:54 -0500

    Simplify membrane
    Now a membrane is just an actor that transparently creates proxies. It doesn't involve local vs remote.

--- tartpy/membrane.py
+++ tartpy/membrane.py
@@ -3,14 +3,7 @@
 Membrane
 ========
 
-A membrane moves messages between configurations of actors.  The
-membrane creates proxy objects for actors before deliverying the
-message to another membrane.
- 
-Membranes deliver and receive messages using arbitrary protocols.  A
-simple "membrane to membrane"" protocol is provided for communicating
-two membranes in the same process. Other protocols can be added by
-implementing a server and a client.
+A membrane transparently creates proxies.
 
 """
 
@@ -28,240 +21,84 @@ from .runtime import behavior, Actor, exception_message
 logger = Logger('membrane')
 
 
-class Membrane(object):
-    """A membrane object.
-
-    Create a membrane with `Membrane(config, runtime)`.  `config` is a
-    dictionary of the form::
-
-        {'protocol': '...',
-         ...data to connect...}
-
-    """
+class MembraneFactory(object):
     
-    def __init__(self, config, runtime):
-        self.config = config
-        if self.config['protocol'] == 'membrane':
-            self.config['target'] = self
-        self.runtime = runtime
-        self.uid_to_proxy = {}
-        self.proxy_to_uid = {}
-        self.start_server()
-
-    def get_proxy(self, uid):
-        """Get proxy associated to ``uid``.
-
-        Raise an exception is there is not associated proxy.
-
-        """
-        proxy = self.uid_to_proxy[uid]
-        self.proxy_to_uid[proxy] = uid
-        return proxy
-
-    def create_proxy(self, uid, remote):
-        """Create proxy for ``uid`` in the ``remote`` membrane."""
-        proxy = self.runtime.create(self.proxy, uid, remote)
-        self.uid_to_proxy[uid] = proxy
-        self.proxy_to_uid[proxy] = uid
-        return proxy
-
-    def get_or_create_proxy(self, uid, remote):
-        """Get or create a new proxy for ``uid`` in the ``remote`` membrane."""
-        try:
-            return self.get_proxy(uid)
-        except KeyError:
-            return self.create_proxy(uid, remote)
-            
-    def get_uid(self, actor):
-        """Convert an actor to a uid."""
-        uid = self.proxy_to_uid.setdefault(actor, uuid.uuid4().hex)
-        self.uid_to_proxy[uid] = actor
-        return uid
-
+    def __init__(self):
+        self.proxy_to_actor = {}
+        self.actor_to_proxy = {}
+        
     @behavior
-    def proxy(self, uid, remote, this, msg):
-        """Behavior for a proxy.
+    def membrane_beh(self, this, message):
+        tag = message.get('tag', 'error')
+        getattr(self, tag)(this, message)
 
-        This actor represents an actor with id `uid` in the membrane
-        `remote`.
+    def _create_proxy(self, this, actor):
+        if actor in self.actor_to_proxy:
+            return self.actor_to_proxy[actor]
+        proxy = this.create(self.proxy_beh, actor)
+        self.proxy_to_actor[proxy] = actor
+        self.actor_to_proxy[actor] = proxy
+        return proxy
 
-        `remote` is the config information to reach the remote
-        membrane.
-
-        """
-        protocol = remote['protocol']
-        client = getattr(self, '{}_client'.format(protocol))
-        ok = client(self.marshall_message(msg), uid, remote)
-        if ok is not True:
-            this.error(ok)
-
-    def start_server(self):
-        protocol = self.config['protocol']
-        server = getattr(self, '{}_server'.format(protocol))
-        server()
-    
-    def local_delivery(self, uid, msg):
-        """Deliver ``msg`` to actor with id ``uid``."""
-        self.uid_to_proxy[uid] << self.unmarshall_message(msg)
-
-    def marshall_actor(self, actor):
-        """Convert an actor to a JSON object.
-
-        Include information to reach back the local membrane.
-
-        """
-        uid = self.get_uid(actor)
-        return {'_proxy': uid,
-                '_config': self.config}
-
-    def unmarshall_actor(self, obj):
-        """Convert a JSON object to a proxy actor."""
-        uid = obj['_proxy']
-        return self.get_or_create_proxy(uid, obj['_config'])
-
-    def is_marshalled_actor(self, obj):
-        """Decide if a JSON object has the form of a marshalled actor."""
-        try:
-            return set(obj.keys()) == {'_proxy', '_config'}
-        except AttributeError:
-            return False
+    def _is_proxy(self, actor):
+        return actor in self.proxy_to_actor
         
-    def unmarshall_message(self, msg):
-        """Import a message.
-
-        Uid identifiers of the form::
-
-            {'_proxy': uid,
-             '_transport': ...}
-
-        are substituted for the proper actor to which they refer.
-
-        """
-        if self.is_marshalled_actor(msg):
-            return self.unmarshall_actor(msg)
-        if isinstance(msg, Mapping):
-            return {key:self.unmarshall_message(value)
-                    for key, value in msg.items()}
-        if isinstance(msg, str):
-            return msg
-        if isinstance(msg, Sequence):
-            return [self.unmarshall_message(value) for value in msg]
-        return msg
-            
-    def marshall_message(self, msg):
-        """Export a message.
-
-        Convert any actor reference to a uid. Proceed recursively to
-        convert references at any level.
-
-        """
-        if isinstance(msg, Actor):
-            return self.marshall_actor(msg)
-        if isinstance(msg, Mapping):
-            return {key:self.marshall_message(value)
-                    for key, value in msg.items()}
-        if isinstance(msg, str):
-            return msg
-        if isinstance(msg, Sequence):
-            return [self.marshall_message(value) for value in msg]
-        return msg
-
-    def membrane_client(self, msg, uid, remote):
-        logger.debug('membrane client')
-        logger.debug(' target: uid: %s' %uid)
-        logger.debug(' at membrane: %s' %remote['target'])
-        logger.debug(' and message is %s' %msg)
-
-        remote['target'].local_delivery(uid, msg)
-        return True
-
-    def membrane_server(self):
-        pass
-
-    def tcp_client(self, msg, uid, remote):
-        """Client for socket connections.
-
-        `remote` has the form::
-
-            {'protocol': 'tcp',
-             'ip': '127.0.0.1',
-             'port': 1234}
-        """
-        ip = remote['ip']
-        port = remote['port']
-        sock = socket.socket()
-        wrapped_msg = {'to': uid,
-                       'msg': msg}
-        try:
-            sock.connect((ip, port))
-            sock_file = sock.makefile('w', encoding='utf8')
-            sock_file.write(json.dumps(wrapped_msg) + '\n')
-            sock_file.close()
-            sock.close()
-            return True
-        except Exception as exc:
-            return exception_message()
-
-    def tcp_server(self):
-        """Server for tcp connections.
- 
-        Use a threaded stream server from the standard library.
- 
-        """
-        class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-            allow_reuse_address = True
- 
-        membrane = self
-        class ThreadedTCPHandler(socketserver.StreamRequestHandler):
-            def handle(self):
-                s = self.rfile.readline().decode('utf-8')
-                if s:
-                    wrapped_msg = json.loads(s)
-                    uid = wrapped_msg['to']
-                    msg = wrapped_msg['msg']
-                    membrane.local_delivery(uid, msg)
- 
-        ip = self.config['ip']
-        port = self.config['port']
-        server = ThreadedTCPServer((ip, port), ThreadedTCPHandler)
-        server_thread = threading.Thread(target=server.serve_forever)
-        server_thread.daemon = True
-        server_thread.start()
+    def create_proxy(self, this, message):
+        """Create proxy for ``uid`` in the ``remote`` membrane."""
+        actor = message['actor']
+        proxy = self._create_proxy(this, actor)
+        message['customer'] << proxy
         
+    @behavior
+    def proxy_beh(self, actor, this, message):
+        actor << self.convert(this, message)
+
+    def convert(self, this, message):
+        if isinstance(message, Actor):
+            if self._is_proxy(message):
+                return message
+            else:
+                return self._create_proxy(this, message)
+        if isinstance(message, Mapping):
+            return {self.convert(this, key): self.convert(this, value)
+                    for key, value in message.items()}
+        if isinstance(message, str):
+            return message
+        if isinstance(message, Sequence):
+            return [self.convert(this, value) for value in message]
+        return message
 
 def test():
-    from .runtime import SimpleRuntime
+    from .runtime import Runtime
     from .example import print_beh, echo_beh
-    from .eventloop import EventLoop
+    
+    runtime = Runtime()
 
-    runtime = SimpleRuntime()
-
+    membrane_inst = MembraneFactory()
+    membrane = runtime.create(membrane_inst.membrane_beh)
+    printer = runtime.create(print_beh)
+    print('printer:', printer)
+    
     @behavior
     def print_echo_beh(self, msg):
         print('echo got', msg)
-        msg['reply_to'] << {'answer': msg}
-        
-    # mb = Membrane({'protocol': 'membrane'}, runtime)
+        msg['customer'] << {'answer': msg}
+
     print_echo = runtime.create(print_echo_beh)
-    # uid_at_mb = mb.get_uid(print_echo)
-
-    # mb2 = Membrane({'protocol': 'membrane'}, runtime)
-    # proxy_for_echo = mb2.create_proxy(uid_at_mb, {'protocol': 'membrane',
-    # 'target': mb})
-    printer = runtime.create(print_beh)
-
-    # proxy_for_echo << {'tag': 5, 'reply_to': printer}
-
-    mb_tcp = Membrane({'protocol': 'tcp', 'ip': 'localhost', 'port': 5555}, runtime)
-    uid_at_mb_tcp = mb_tcp.get_uid(print_echo)
-
-    mb_tcp_2 = Membrane({'protocol': 'tcp', 'ip': 'localhost', 'port': 6666}, runtime)
-    proxy_for_echo2 = mb_tcp_2.create_proxy(uid_at_mb_tcp, mb_tcp.config)
-
-    proxy_for_echo2 << {'tag': 5, 'reply_to': printer}
+    print('print_echo:', print_echo)
     
-    evloop = EventLoop()
-    
-    return locals()
+    @behavior
+    def go_beh(self, msg):
+        if msg == 'go':
+            membrane << {'tag': 'create_proxy',
+                         'actor': print_echo,
+                         'customer': self}
+        else:
+            print('got back:', msg)
+            msg << {'customer': printer}
+
+    go = runtime.create(go_beh)
+    go << 'go'
+    return membrane_inst
 
     
--- tartpy/tests/test_membrane.py
+++ tartpy/tests/test_membrane.py
@@ -4,15 +4,14 @@ import pytest
 
 from tartpy.runtime import SimpleRuntime, behavior
 from tartpy.eventloop import EventLoop
-from tartpy.membrane import Membrane
-
+from tartpy.membrane import MembraneFactory
 
 def test_membrane_protocol():
     runtime = SimpleRuntime()
     evloop = EventLoop()
-    
-    m1 = Membrane({'protocol': 'membrane'}, runtime)
-    m2 = Membrane({'protocol': 'membrane'}, runtime)
+
+    membrane_inst = MembraneFactory()
+    membrane = runtime.create(membrane_inst.membrane_beh)
 
     result1 = None
     @behavior
@@ -29,92 +28,47 @@ def test_membrane_protocol():
     actor1 = runtime.create(actor1_beh)
     actor2 = runtime.create(actor2_beh)
 
-    uid_for_2_at_mb2 = m2.get_uid(actor2)
-    proxy_for_2_at_mb1 = m1.create_proxy(uid_for_2_at_mb2,
-                                         m2.config)
-    
-    proxy_for_2_at_mb1 << {'foo': 5,
-                           'reply_to': actor1}
-    
+    actor2_proxy = None
+    @behavior
+    def go_beh(self, msg):
+        nonlocal actor2_proxy
+        if msg == 'go':
+            membrane << {'tag': 'create_proxy',
+                         'actor': actor2,
+                         'customer': self}
+        else:
+            actor2_proxy = msg
+
+    go = runtime.create(go_beh)
+    go << 'go'
+
     evloop.run()
 
-    # test message from m1 to m2
+    actor2_proxy << {'foo': 5,
+                     'customer': actor1}
+
+    evloop.run()
+
+    # test message from actor1 to actor2
     assert result2['foo'] == 5
 
-    # test that 'reply_to' is a proxy at m2
-    proxy_for_1_at_mb2 = result2['reply_to']
-    assert proxy_for_1_at_mb2 is not actor1
+    # test that customer is really a proxy
+    assert result2['customer'] is not actor1
 
-    proxy_for_1_at_mb2 << {'bar': 3,
-                           'reply_to': actor2}
+    actor1_proxy = result2['customer']
+    actor1_proxy << {'bar': 3,
+                     'customer': actor2}
 
     evloop.run()
 
-    # test message back from m2 to m1
+    # test message from actor2 to actor1
     assert result1['bar'] == 3
 
-    # test that proxy at m1 is reused
-    assert result1['reply_to'] is proxy_for_2_at_mb1
+    # test that proxy of actor2 is reused
+    assert result1['customer'] is actor2_proxy
 
-    # test a string message across Membranes
-    proxy_for_2_at_mb1 << 'a string message'
+    # test a string message
+    actor2_proxy << 'a string message'
     evloop.run()
     assert result2 == 'a string message'
 
-def test_dos():
-    runtime = SimpleRuntime()
-    m = Membrane({'protocol': 'membrane'}, runtime)
-    with pytest.raises(KeyError):
-        m.local_delivery(0, {})
-
-def test_marshall_unmarshall():
-    runtime = SimpleRuntime()
-    m = Membrane({'protocol': 'membrane'}, runtime)
-
-    assert m.marshall_message(5) == 5
-    assert m.marshall_message('foo') == 'foo'
-    assert m.marshall_message([1, 2, 'bar']) == [1, 2, 'bar']
-    assert m.marshall_message({'foo': 5, 'bar': 'baz'}) == {'foo': 5, 'bar': 'baz'}
-
-    assert m.unmarshall_message(5) == 5
-    assert m.unmarshall_message('foo') == 'foo'
-    assert m.unmarshall_message([1, 2, 'bar']) == [1, 2, 'bar']
-    assert m.unmarshall_message({'foo': 5, 'bar': 'baz'}) == {'foo': 5, 'bar': 'baz'}
-
-    @behavior
-    def sink_beh(self, msg):
-        pass
-    sink = runtime.create(sink_beh)
-    
-    s = m.marshall_message(sink)
-    assert m.is_marshalled_actor(s)
-    assert m.unmarshall_message(s) is sink
-
-    s = m.marshall_message({'foo': sink})
-    assert m.is_marshalled_actor(s['foo'])
-    assert m.unmarshall_message(s)['foo'] is sink
-
-    s = m.marshall_message([sink])
-    assert m.is_marshalled_actor(s[0])
-    assert m.unmarshall_message(s)[0] is sink
-
-def test_client_error():
-    
-    error_msg = None
-    class MyRuntime(SimpleRuntime):
-        def throw(self, msg):
-            nonlocal error_msg
-            error_msg = msg
-
-    runtime = MyRuntime()
-    
-    mb = Membrane({'protocol': 'tcp', 'ip': 'localhost', 'port': 0},
-                  runtime)
-    proxy = mb.create_proxy(0, mb.config)
-    proxy << 5
-
-    evloop = EventLoop()
-    evloop.run()
-
-    assert isinstance(error_msg, Mapping)
-    assert set(error_msg.keys()) == {'exception', 'traceback'}
