7aae1b61cf7a732da87bce82626be5aab4fe631b
Walter Moreira <walter@waltermoreira.net>
2014-04-14 14:34:18 -0500

    Bump to version 0.2.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 setup(name='tartpy',
-      version='0.1.0',
+      version='0.2.0',
       description='Tiny Actor Run-Time in Python.',
       url='http://github.com/waltermoreira/tartpy',
       author='Walter Moreira',
