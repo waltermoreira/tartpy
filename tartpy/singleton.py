ef1c8a095ab43b9bb068bccd3de1984ee67e87c8
Walter Moreira <walter@waltermoreira.net>
2014-04-24 15:30:56 -0500

    Make singleton more robust
    It should allow to create multiple instances according to the arguments for __init__.

--- tartpy/singleton.py
+++ tartpy/singleton.py
@@ -15,10 +15,9 @@ class Singleton(type):
     
     def __init__(self, name, bases, namespace):
         super().__init__(name, bases, namespace)
-        self.instance = None
+        self.instances = {}
 
     def __call__(self, *args, **kwargs):
-        if self.instance is None:
-            self.instance = super().__call__(*args, **kwargs)
-        return self.instance
+        return self.instances.setdefault(
+            args, super().__call__(*args, **kwargs))
             
